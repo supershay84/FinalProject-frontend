35db8cae27120d65b87bf5bdd2319af4522934bb
theaharris <supershay84@gmail.com>
2021-01-29 16:23:52 -0500

    React app created. Material UI installed. Initial styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// COMPONENTS
+import NavBar from './Components/NavBar';
+// PAGES
+import home from './Pages/home';
+import login from './Pages/login';
+import signup from './Pages/signup';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+       <NavBar/>
+       <div className="container">
+        <Switch>
+          <Route exact path="/" component={home}/>
+          <Route exact path="/login" component={login}/>
+          <Route exact path="/signup" component={signup}/>
+        </Switch>
+       </div>
+      </Router>
     </div>
   );
 }
