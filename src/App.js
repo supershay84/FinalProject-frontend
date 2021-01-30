c0d7ac7385f48fc67e4cd4743ec3a772106be02c
theaharris <supershay84@gmail.com>
2021-01-29 19:06:03 -0500

    More styling. Images do not appear.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 // COMPONENTS
 import NavBar from './Components/NavBar';
 // PAGES
@@ -7,9 +9,24 @@ import home from './Pages/home';
 import login from './Pages/login';
 import signup from './Pages/signup';
 
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: '#212121'
+      },
+      secondary: {
+        main: '#311b92'
+      }
+    },
+    typography: {
+      useNextVariants: true
+    }
+})
+
 function App() {
   return (
-    <div className="App">
+    <MuiThemeProvider theme ={theme}>
+      <div className="App">
       <Router>
        <NavBar/>
        <div className="container">
@@ -21,6 +38,7 @@ function App() {
        </div>
       </Router>
     </div>
+    </MuiThemeProvider>
   );
 }
 
---
+++ src/Components/Scream.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import withStyles from '@material-ui/core/styles/withStyles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import { Typography } from '@material-ui/core';
+import Link from 'react-router-dom/Link';
+
+const styles = {
+    card: {
+        display: 'flex',
+        marginBottom: 20
+    },
+    image: {
+        minWidth: 200
+    },
+    content: {
+        padding: 25
+    }
+}
+
+class Scream extends Component {
+    render() {
+        const { classes, scream : { body, createdAt, userImage, userHandle, screamId, likeCount, commentCount } } = this.props;
+        return (
+           <Card className={classes.card}>
+               <CardMedia
+               className={classes.image} 
+               image={userImage}
+               title="Profile image"/>
+               <CardContent className={classes.content} >
+                   <Typography 
+                    variant="h5" 
+                    component={Link} 
+                    to={`/users/${userHandle}`} 
+                    color="secondary">{userHandle}</Typography> 
+                   <Typography 
+                    variant="body2" 
+                    color="textSecondary">{createdAt}</Typography>
+                   <Typography 
+                    variant="body1">{body}</Typography>
+               </CardContent>
+           </Card>
+        )
+    }
+}
+
+export default withStyles(styles)(Scream);
