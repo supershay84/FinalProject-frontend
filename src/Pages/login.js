755b409f8f0b2d7e84c3b2c5aafd8042a481926c
theaharris <supershay84@gmail.com>
2021-02-01 00:42:29 -0500

    Login page styling started

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
+
+const styles = {
+    form: {
+        textAlign: 'center'
+    }
+};
 
 export class login extends Component {
     render() {
+        const { classes } = this.props
         return (
-            <div>
-                <h1>Login</h1>
-            </div>
+           <Grid container className={classes.form}>
+               <Grid item sm/>
+               <Grid item sm>
+                   <p>Helloooooo</p>
+               </Grid>
+               <Grid item sm/>
+           </Grid>
         )
     }
 }
 
-export default login
+login.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+export default withStyles(styles)(login);
